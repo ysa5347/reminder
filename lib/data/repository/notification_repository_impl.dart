cb43cc8dc33025531ad655cc5fad93adee2cf1ba
ysa5347 <ysa5347@gmail.com>
2025-05-31 23:09:51 +0900

    EDIT; edit file/dir structure after merging, added example of entities (4)

--- lib/data/repository/notification_repository_impl.dart
+++ lib/data/repository/notification_repository_impl.dart
@@ -13,24 +13,6 @@ class NotificationRepositoryImpl extends NotificationRepository {
   }
 
   @override
-  Future<List<Item>> GetItemsById(int itemId) async {
-    // TODO: itemId를 통해 데이터베이스에서 데이터를 가져옴
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Item> SaveItem(Item item) async {
-    // TODO: item을 데이터베이스에 저장
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Item> DeleteItem(int itemId) async {
-    // TODO: itemId를 통해 데이터베이스에서 데이터를 삭제
-    throw UnimplementedError();
-  }
-
-  @override
   Future<Notification> SaveNotification(Notification notification) async {
     // TODO: notification을 데이터베이스에 저장
     throw UnimplementedError();
